67b378ebb190c1ed389b692cae8692defd0cc654
oshnic <oshnicchhetri75@gmail.com>
2025-01-12 18:23:22 +0100

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,12 @@ import cartRoutes from "./routes/cart.route.js";
 import orderRoutes from "./routes/order.route.js";
 
 import cookieParser from "cookie-parser";
-// import path from "path"
+import path from "path"
+import e from "express";
 
 dotenv.config();
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json({limit: "10mb"}));
@@ -27,6 +28,10 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/coupon", couponRoutes);
 app.use("/api/order", orderRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
 
 
 app.listen(PORT, () => {
