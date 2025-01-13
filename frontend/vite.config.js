c22c4609a686da1cc31adc5279b4707ae60bd787
oshnicChhetri <130503137+oshnicChhetri@users.noreply.github.com>
2025-01-13 02:32:18 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: "dist", // The build output directory
   },
   server: {
     proxy: {
-      // Replace "/api" with the base path for your API requests
-      '/api': {
-        target: 'https://e-commerce-jskt.vercel.app/', // Replace with your backend server's URL
+      // Proxy requests starting with "/api" to your backend
+      "/api": {
+        target: "https://e-commerce-jskt.vercel.app/", // Replace with your backend server's URL
         changeOrigin: true, // Needed for virtual hosted sites
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove "/api" prefix if your backend doesn't use it
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: Remove "/api" prefix if your backend doesn't use it
       },
     },
   },
