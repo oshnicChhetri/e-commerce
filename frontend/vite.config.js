bc5e2f404e116c5ba77f02f813dfe9d61ae16051
oshnicChhetri <130503137+oshnicChhetri@users.noreply.github.com>
2025-01-13 02:12:41 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Replace "/api" with the base path for your API requests
       '/api': {
-        target: 'https://e-commerce-jskt.vercel.app/', // Replace with your backend server's URL
+        target: 'https://ecommercefrontend20-abspqe205-oshnics-projects.vercel.app/', // Replace with your backend server's URL
         changeOrigin: true, // Needed for virtual hosted sites
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove "/api" prefix if your backend doesn't use it
       },
