81be1ec1dc23d19e9ce54c9bf900a6e76fdcbffc
oshnicChhetri <130503137+oshnicChhetri@users.noreply.github.com>
2025-01-18 13:52:16 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,52 +8,26 @@ import paymentRoutes from "./routes/payment.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import orderRoutes from "./routes/order.route.js";
-
 import cookieParser from "cookie-parser";
-import path from "path";
 
 dotenv.config();
 
-// const __dirname = path.resolve();
 const app = express();
 
 // Middleware to parse JSON and cookies
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+app.use(cors());
 
 // Connect to MongoDB
 connectToMongoDb();
 
-// Define the port for the server
-const PORT = process.env.PORT || 8000;
-
 // API Routes
-app.use("/api/auth/", authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/coupon", couponRoutes);
 app.use("/api/order", orderRoutes);
 
-// Serve the frontend (built with React or any other SPA framework)
-<<<<<<< HEAD
-// app.use(express.static(path.join(__dirname, "/frontend/public")));
-
-// Catch-all route to serve the frontend app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
-// });
-=======
-app.use(express.static(path.join(__dirname, "/frontend/public")));
-app.use(cors());
-
-// Catch-all route to serve the frontend app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
-});
->>>>>>> ddcf52b810f6b40e6aad20b97b6dffdd69dbdd55
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app; // Export the app for serverless deployment
